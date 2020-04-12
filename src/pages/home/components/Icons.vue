ae62410648dd9737030b6660c0ea9a164244e9bc
mataotao <mttwind@foxmail.com>
2020-04-12 20:23:43 +0800

    7-4icon部分内容

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="icons">
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="https://s.qunarzz.com/homenode/images/touchheader/piao.png" alt="">
+      </div>
+      <p class="icon-desc">景点门票</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  mounted () {
+
+  }
+}
+</script>
+
+<style scoped lang="less">
+  @import "~styles/varibles.less";
+  /*先占位*/
+  .icons{
+    overflow: hidden;
+    width: 100%;
+    height: 0;
+    padding-bottom: 50%;
+    .icon{
+      position: relative;
+      overflow: hidden;
+      height: 0;
+      float: left;
+      width:25%;
+      padding-bottom: 25%;
+      .icon-img{
+        position: absolute;
+        top:0;
+        left: 0;
+        right: 0;
+        bottom: .44rem;
+        box-sizing: border-box;
+        padding: .1rem;
+        .icon-img-content{
+          display: block;
+          margin: 0 auto;
+          height: 100%;
+        }
+      }
+      .icon-desc{
+        position: absolute;
+        bottom:0;
+        left: 0;
+        right: 0;
+        height: .44rem;
+        line-height: .44rem;
+        color:@darkColor;
+        text-align: center;
+      }
+    }
+  }
+</style>
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -2,9 +2,9 @@
   <div class="wrapper">
     <swiper :options="swiperOption" ref="mySwiper">
       <swiper-slide v-for="item of swiperList" :key="item.id">
-        <img class="swiper-img" :src="item.imgUrl" alt="">
+        <img class="swiper-img" :src="item.imgUrl" alt="" />
       </swiper-slide>
-      <div class="swiper-pagination"  slot="pagination"></div>
+      <div class="swiper-pagination" slot="pagination"></div>
     </swiper>
   </div>
 </template>
@@ -18,18 +18,22 @@ export default {
         pagination: '.swiper-pagination',
         loop: true
       },
-      swiperList: [{
-        id: '0001',
-        imgUrl: 'https://imgs.qunarzz.com/vc/44/e9/86/95bc36c9e1c06ebd68bdfe222e.jpg_92.jpg'
-      },
-      {
-        id: '0002',
-        imgUrl: 'https://imgs.qunarzz.com/vc/6d/9f/35/b8ad5468f73fd60ec0ced086f6.jpg_92.jpg'
-      },
-      {
-        id: '0003',
-        imgUrl: 'https://imgs.qunarzz.com/vc/e3/a1/71/f498dfd3bed948d623c9093252.jpg_92.jpg'
-      }
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl:
+            'https://imgs.qunarzz.com/vc/44/e9/86/95bc36c9e1c06ebd68bdfe222e.jpg_92.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl:
+            'https://imgs.qunarzz.com/vc/6d/9f/35/b8ad5468f73fd60ec0ced086f6.jpg_92.jpg'
+        },
+        {
+          id: '0003',
+          imgUrl:
+            'https://imgs.qunarzz.com/vc/e3/a1/71/f498dfd3bed948d623c9093252.jpg_92.jpg'
+        }
       ]
     }
   },
@@ -46,19 +50,19 @@ export default {
 </script>
 
 <style scoped lang="less">
-  @import '~styles/varibles.less';
-  .wrapper /deep/ .swiper-pagination-bullet-active{
-    background: #fff;
-  }
-  .wrapper{
-    overflow: hidden;
-    width: 100%;
-    height: 0;
-    padding-bottom: 30%;
-    background: #ccc;
-    /*  始终保持外部宽高比违3:10*/
-  }
-  .swiper-img{
-    width: 100%;
-  }
+@import "~styles/varibles.less";
+.wrapper /deep/ .swiper-pagination-bullet-active {
+  background: #fff;
+}
+.wrapper {
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  padding-bottom: 30%;
+  background: #ccc;
+  /*  始终保持外部宽高比违3:10*/
+}
+.swiper-img {
+  width: 100%;
+}
 </style>
