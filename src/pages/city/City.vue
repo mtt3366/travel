c2963457c79b14b14d3b5d54f8260651225d48ac
mataotao <mttwind@foxmail.com>
2020-04-22 14:37:24 +0800

    8-1 城市选择页面路由配置

---
+++ src/pages/city/City.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <city-header></city-header>
+  </div>
+</template>
+
+<script>
+import CityHeader from './cpmponents/Header'
+import axios from 'axios'
+export default {
+  name: 'City',
+  components: {
+    CityHeader
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  mounted () {
+
+  }
+}
+</script>
+
+<style>
+</style>
