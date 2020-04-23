ec8583a7ee44f82a7065bcafa745d6440f34c9a6
mataotao <mttwind@foxmail.com>
2020-04-23 16:37:18 +0800

    8-9 Vuex实现数据共享3使用localstorage ,vuex的拆分为不同的模块

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import state from './state'
+import mutations from './mutations'
 Vue.use(Vuex)
 
 // create new Store and export 创建新的仓库并且导出
 export default new Vuex.Store({
-  state: {// place global public data 放置全局公用数据
-    city: '上海'
-  },
-  // actions: {
-  //   changeCity (ctx, city) {
-  //     // use ctx get 'commit' method  使用ctx拿到commit方法
-  //     ctx.commit('changeCityx', city)// 执行mutations
-  //   }
-  // },
-  mutations: {
-    changeCity (state, city) { // store 里面的state 和 传过来的city
-      state.city = city// 根据传过来的city,改变 store 里面的state
-    }
-  }
+  state,
+  mutations
 })
---
+++ src/store/mutations.js
@@ -0,0 +1,9 @@
+const mutations = {
+  changeCity (state, city) {
+    state.city = city
+    try {
+      localStorage.city = city
+    } catch (e) {}
+  }
+}
+export default mutations
