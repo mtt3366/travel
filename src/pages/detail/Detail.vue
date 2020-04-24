6975278c7682e0b6facae5fe54c6aad3cf454c07
mataotao <mttwind@foxmail.com>
2020-04-24 14:28:00 +0800

    9-3 Vue项目详情页 - 实现Header渐隐渐显效果

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,19 +1,24 @@
 <template>
-  <div>
+  <div class="wrapper">
     <detail-banner></detail-banner>
+    <detail-header></detail-header>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
 export default {
   name: 'Detail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
 
 <style scoped lang="stylus">
   @import "~styles/mixins.styl";
+  .wrapper
+    height 50rem
 </style>
